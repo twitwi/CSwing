3a7e959af61c5e09e8959c8ae6296e5e98276150
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-11-11 01:50:07 +0100

    first replacement ok

---
+++ apply-mapping.sh
@@ -0,0 +1,42 @@
+
+lang=$1
+
+sedopt=$(  cat map-$lang | awk "{rest=\$2; for (i=3;i<=NF;i++) {rest = rest \" \" \$i}; printf( \"-e 's@%s@%s@g' \", \$1, rest) }"  )
+
+
+# remove old one
+test -d $lang && rm -rf $lang
+
+# copy git controlled files only
+cd abstract
+git ls-files . | while read i ; do
+    mkdir -p ../$lang/$(dirname $i)
+    cp $i ../$lang/$i
+done
+cd -
+
+# do replacement
+find $lang/ -type f | while read i; do
+    echo "Processing '$i'"
+    eval set -- $sedopt
+    sed -i "$@" "$i"
+done
+
+# do file renaming
+find $lang/ -type f | while read i; do
+    eval set -- $sedopt
+    ii=$(echo "$i" | sed "$@")
+    if test "$i" != "$ii" ; then
+        echo "Moving '$i' ==> '$ii'"
+        mv "$i" "$ii"
+    fi
+done
+
+
+
+# build and test
+export JVMDLL=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server/libjvm.so
+export CSWINGJAR=../CSwingJava/target/CSwingJava-1.1-SNAPSHOT.jar
+
+(cd $lang/CSwingJava/ && mvn install)
+(cd $lang/cswing/ && gcc -I/usr/lib/jvm/java-1.7.0-openjdk-amd64/include/ -I/usr/lib/jvm/java-1.7.0-openjdk-amd64/include/linux/ test-cswing.c cswing.c javahelper.c -L/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre/lib/amd64/server -ldl -o ,,test && ./,,test && rm -f ,,test)
